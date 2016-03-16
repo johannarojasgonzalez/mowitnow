e9f91c8161d57821cf76498f7533ede579acbe4a
jrofball <johannarojasgon@gmail.com>
2016-03-16 22:22:29 +0100

    Injection service par @Autowired
    Injection service par @Autowired et non par l'application context

--- src/main/java/com/itn/mowitnow/mower/ui/controller/MowerController.java
+++ src/main/java/com/itn/mowitnow/mower/ui/controller/MowerController.java
@@ -2,8 +2,7 @@ package com.itn.mowitnow.mower.ui.controller;
 
 import static org.springframework.util.CollectionUtils.isEmpty;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.itn.mowitnow.mower.beans.InfoGameBean;
 import com.itn.mowitnow.mower.beans.MowerBean;
 import com.itn.mowitnow.mower.beans.PositionBean;
-import com.itn.mowitnow.mower.services.impl.MowerMouvementServiceImpl;
+import com.itn.mowitnow.mower.services.IMowerMouvementService;
 
 @Controller
 public class MowerController {
 
+	@Autowired
+	private IMowerMouvementService mouvementService;
+	
 	@RequestMapping("/rungame")
 	public String resultgame(Model model) throws Exception {
 		model.addAttribute("resultat", getResultRunGame());
@@ -28,9 +30,6 @@ public class MowerController {
 	 */
 	private String getResultRunGame() {
 		StringBuilder sb = new StringBuilder();
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
-		MowerMouvementServiceImpl mouvementService = ctx.getBean(MowerMouvementServiceImpl.class);
-
 		InfoGameBean infoGameBean;
 		try {
 			infoGameBean = mouvementService.loadDataFromFile();
